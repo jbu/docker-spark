0753cdf0f5f02f3047882d328d10b085fb7b8206
James Uther <james@lshift.net>
2015-04-22 11:10:10 +0100

    giving in and adding start scripts

---
+++ master/start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export SPARK_MASTER_PORT=${SPARK_MASTER_SERVICE_PORT:-7077}
+
+java -cp /spark/conf:/spark/lib/* \
+     -Dspark.akka.logLifecycleEvents=true \
+     -Xms512m -Xmx512m \
+     org.apache.spark.deploy.master.Master \
+     -h $SPARK_MASTER_SERVICE_HOST
---
+++ worker/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#echo nameserver $KUBE_DNS_SERVICE_HOST >> /etc/resolv.conf
+
+java -cp /spark/conf:/spark/lib/* \
+     -Dspark.akka.logLifecycleEvents=true \
+     -Xms512m -Xmx512m \
+     org.apache.spark.deploy.worker.Worker \
+     spark://$SPARK_MASTER_SERVICE_HOST:$SPARK_MASTER_SERVICE_PORT
+
+     #spark://spark-master:7070
